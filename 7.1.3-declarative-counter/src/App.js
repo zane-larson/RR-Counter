9dfa677e2eb2b3917338020b4bf299aff4b7e80d
zane-larson <zanelarson2002@gmail.com>
2022-12-03 18:41:39 -0800

    first

---
+++ 7.1.3-declarative-counter/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+function App(){
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
+    
+  )
+}
+
+export default App;
